20450816fb6bc08955463dff77e83a5b23cc9f28
Amine <wissarmohamedamine@gmail.com>
2023-12-22 18:07:49 +0100

    still on the menu component

--- src/components/2-hero/Hero.jsx
+++ src/components/2-hero/Hero.jsx
@@ -1,7 +1,30 @@
-import React from "react";
 import "./hero.css";
 const Hero = () => {
-  return <div>hero</div>;
+  return (
+    <section className="hero flex">
+      <div className="left-section">
+        <div className="parent-avatar flex">
+          <img src="./amine.png" alt="" className="avatar" />
+          <div className="icon-verified"></div>
+        </div>
+
+        <h1 className="title">Fullstack-Developer</h1>
+        <p className="sub-title">
+          I'm a Web Developer with a vast experience in the field, I have worked
+          in different field with companies and small businesses, that allowed
+          me to understand and master the need of the market
+        </p>
+        <div className="all-icons flex">
+          <div className="icon icon-twitter"></div>
+          <div className="icon icon-instagram"></div>
+          <div className="icon icon-github"></div>
+          <div className="icon icon-linkedin"></div>
+        </div>
+      </div>
+      <div className="right-section animation border">ccc</div>
+    </section>
+  );
+  // stopped at 2:00:32
 };
 
 export default Hero;
--- src/components/3-main/Main.jsx
+++ src/components/3-main/Main.jsx
@@ -1,7 +1,36 @@
-import React from "react";
 import "./main.css";
 const Main = () => {
-  return <div>main</div>;
+  return (
+    <main className="flex">
+      <section className="left-section flex">
+        <button className="active">all projects</button>
+        <button>html & css</button>
+        <button>JavaScript</button>
+        <button>React & Mui</button>
+        <button>Node & express</button>
+      </section>
+      <section className="right-section">
+        <article className="card">
+          <img src="./project1.png" alt="" width={266} />
+          <div style={{ width: "266px" }} className="border box">
+            <h1 className="title">Landing Page 2</h1>
+            <p className="sub-title">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
+              eveniet commodi nulla cum. Aliquid saepe, iusto beatae doloribus
+              sint eaque!
+            </p>
+            <div className="flex icons">
+              <div className="icon-link"></div>
+              <div className="icon-github"></div>
+              <a href="">
+                more <span className="icon-arrow_forward"></span>
+              </a>
+            </div>
+          </div>
+        </article>
+      </section>
+    </main>
+  );
 };
 
 export default Main;
