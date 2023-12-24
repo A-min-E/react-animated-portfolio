55ab3421063efc6ffe36476f8f45b0e8861776c4
Amine <wissarmohamedamine@gmail.com>
2023-12-24 01:36:07 +0100

    main finished

--- src/components/3-main/Main.jsx
+++ src/components/3-main/Main.jsx
@@ -9,25 +9,35 @@ const Main = () => {
         <button>React & Mui</button>
         <button>Node & express</button>
       </section>
-      <section className="right-section">
-        <article className="card">
-          <img src="./project1.png" alt="" width={266} />
-          <div style={{ width: "266px" }} className="border box">
-            <h1 className="title">Landing Page 2</h1>
-            <p className="sub-title">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
-              eveniet commodi nulla cum. Aliquid saepe, iusto beatae doloribus
-              sint eaque!
-            </p>
-            <div className="flex icons">
-              <div className="icon-link"></div>
-              <div className="icon-github"></div>
-              <a href="">
-                more <span className="icon-arrow_forward"></span>
-              </a>
-            </div>
-          </div>
-        </article>
+      <section className="flex right-section">
+        {["aa", "bb", "cc", 1, 2].map((item) => {
+          return (
+            <article className="card" key={item}>
+              <img src="./project1.png" alt="" width={266} />
+              <div style={{ width: "266px" }} className="box">
+                <h1 className="title">Landing Page 2</h1>
+                <p className="sub-title">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
+                  eveniet commodi nulla cum. Aliquid saepe, iusto beatae
+                  doloribus sint eaque!
+                </p>
+                <div className="flex icons">
+                  <div className="flex" style={{ gap: "11px" }}>
+                    <div className="icon-link"></div>
+                    <div className="icon-github"></div>
+                  </div>
+                  <a href="" className="link flex">
+                    more{" "}
+                    <span
+                      style={{ alignSelf: "end" }}
+                      className="icon-arrow_forward"
+                    ></span>
+                  </a>
+                </div>
+              </div>
+            </article>
+          );
+        })}
       </section>
     </main>
   );
