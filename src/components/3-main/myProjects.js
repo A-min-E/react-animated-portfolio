f11f99a13aeee32300492995ba877bc78dae7244
Amine <wissarmohamedamine@gmail.com>
2024-01-15 02:12:13 +0100

    adding some other websites to the main secetion

--- src/components/3-main/myProjects.js
+++ src/components/3-main/myProjects.js
@@ -1,26 +1,26 @@
 export const myProjects = [
   {
-    projectTitle: "react project",
+    projectTitle: "react & tailwind project",
     category: ["react"],
     imgPath: "./project1.png",
     webSite: "https://nike-shop.rf.gd/?i=1",
   },
   {
-    projectTitle: "html & css & js project",
-    category: ["js"],
-    imgPath: "./project2.png",
-    webSite: "https://maw-fitness-club.000webhostapp.com/",
-  },
-  {
-    projectTitle: "html & css & js project",
-    category: ["js"],
-    imgPath: "./project1.png",
-    webSite: "https://nike-shop.rf.gd/?i=1",
-  },
-  {
     projectTitle: "react & tailwind project",
-    category: ["tailwind", "react"],
+    category: ["react"],
     imgPath: "./project2.png",
     webSite: "https://maw-fitness-club.000webhostapp.com/",
+  },
+  {
+    projectTitle: "html & css & js project",
+    category: ["js"],
+    imgPath: "./project3.png",
+    webSite: "https://maw-coffe-shop.000webhostapp.com/",
+  },
+  {
+    projectTitle: "html & css & js project",
+    category: ["js"],
+    imgPath: "./project4.png",
+    webSite: "https://aminestoremaroc.000webhostapp.com/",
   },
 ];
